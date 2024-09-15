0fdc793b79e9f8ec8f08edc257104167a427726a
Kuldip Sirpali <kuldipsirpali779@gmail.com>
2024-09-15 20:00:05 +0545

    change

--- JS/DataHaldle.js
+++ JS/DataHaldle.js
@@ -7,7 +7,7 @@ validateEmailButton.addEventListener("click", (evt) => {
   emailValue = setEmailInput.value;
   const isValidEmail = /\S+@\S+\.\S+/.test(emailValue);
   if (isValidEmail) {
-    window.location.href = "video-app-frontend/HTML/signUp.html";
+    window.location.href = "./HTML/signUp.html";
     sessionStorage.setItem("email", emailValue); //to store data in session storage
   } else {
     alert("Invalid email. Please enter a valid email address.");
